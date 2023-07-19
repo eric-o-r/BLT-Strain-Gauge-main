23207b0cf69b8a8b9f12a9c40888c1c0915c88e5
eric-o-r <93245420+eric-o-r@users.noreply.github.com>
2023-07-18 23:51:55 -0700

    Datalogging V1

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
+File myFile;
+
 int sensorValue = analogRead(A0);
 int sensorValue2 = analogRead(A1);
 int sensorValue3 = analogRead(A2);
@@ -8,6 +12,8 @@ void setup() {
   Serial.begin(9600);
   
 }
+
+
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0:
@@ -18,6 +24,25 @@ void loop() {
   float voltage = sensorValue * (5.0 / 1023.0);
    float voltage2 = sensorValue2 * (5.0 / 1023.0);
       float voltage3 = sensorValue3 * (5.0 / 1023.0);
+
+
+
+while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(BUILTIN_SDCARD)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+
+myFile = SD.open("strain_gauge_votlage_drop_data.txt", FILE_WRITE);
+
+
 
 // Measures voltage across bridge
 Serial.print(" Voltage difference: ");
@@ -35,4 +60,33 @@ Serial.print(" Voltage difference with Op Amp: ");
 
 //Place holder code for the formula to obtain strain from change in resistance  measured above
 //strain = ((Rx - defualt R of strain gauge)/Default R of gauge) / gage factor
+
+if (myFile) {
+    Serial.print("Writing to test.txt...");
+        myFile.println("Voltage Difference:");
+    myFile.println(voltage2- voltage);
+    myFile.println("Voltage Difference W Op Amp: ");
+    myFile.println(voltage3);
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
 }
